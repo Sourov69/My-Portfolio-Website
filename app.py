77598d0f550c2205cdb4484ec319a40c3e2c811e
Sourovtalukder69 <sourovtalukder69@gmail.com>
2025-11-13 19:22:35 +0600

    First try to upload portfolion on github

---
+++ app.py
@@ -0,0 +1,133 @@
+import streamlit as st
+import base64
+
+st.set_page_config(page_title="Sourov Talukder | Portfolio", layout="wide")
+
+st.image("Profile top.png")
+st.write("Ecommerce Dashboard Project")
+st.image("Screenshot (249).png")
+
+st.title("Advance Finantial Project", width="stretch")
+st.image("Screenshot (245).png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if st.checkbox("Show Project Details"):
+ 
+#     # You can use a column just like st.sidebar:
+
+#     # Or even better, call Streamlit functions inside a "with" block:
+
+#     chosen = st.radio(
+#         'Sorting hat',
+#         ("Sales", "Product", "Customer", "Other"),horizontal=True)
+        
+
+  
+#     if chosen ==  "Sales":
+#         st.image("Screenshot (250).png")
+#     elif chosen == "Product":
+#         st.image("Screenshot (245).png")
+#     elif chosen == "Customer":
+#         st.video("My Hridhy.mp4")
+#     else:
+#         st.image("My pic.png")
